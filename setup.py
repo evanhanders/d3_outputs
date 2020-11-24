6c5161cf0dccf34c4eff503bc69bf56e44997cbf
Evan H. Anders <evanhanders@gmail.com>
2020-11-24 10:33:05 -0800

    makes basic package files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='d3_outputs',
+      version='1.0.0',
+      description='A plugin for creating specialized output tasks for simulations on the d3 branch of dedalus',
+      url='https://github.com/evanhanders/de_outputs',
+      author='Evan Anders',
+      author_email='evan.anders@northwestern.edu',
+      license='GPL-3.0',
+      packages=['d3_outputs'],
+      zip_safe=False)
